d4a7a24ad314241872f32b2fbf44ebcdc9423d92
basicjohn <edmondsonj@gmail.com>
2022-01-10 11:21:30 -0800

    update App & index into ts files

--- __tests__/clients/InstitutionClientTests.js
+++ __tests__/clients/InstitutionClientTests.js
@@ -1,19 +1,22 @@
 import InstitutionClient from '../../src/clients/InstitutionClient';
 
-describe("InstitutionClient Search", () => {
+describe("InstitutionClient.search", () => {
   test("Run search function that returns no result", () => {
     let testSearch = "***";
     return InstitutionClient.search(testSearch).then(response => {
-      console.log(response);
-      expect(response.data.length).toBe(1);
+      expect(response.data.length).toBe(0);
     });
   });
 
   test("Run search function that returns result", () => {
     let testSearch = "new";
     return InstitutionClient.search(testSearch).then(response => {
-      console.log(response);
       expect(response.data.length).toBeGreaterThan(0);
     });
   });
+});
+
+
+describe("InstitutionClient.favorite", () => {
+  
 });
