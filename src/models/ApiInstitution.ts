10fbdfa888bfbfdb82928ac921bc1dc2e4ecb791
basicjohn <edmondsonj@gmail.com>
2022-01-09 18:57:37 -0800

    update optional fields in data models

--- src/models/ApiInstitution.ts
+++ src/models/ApiInstitution.ts
@@ -1,8 +1,8 @@
 import ApiInstitutionData from "./ApiInstitutionData";
-import ApiHighlight from "./ApiHighlight";
+// import ApiHighlight from "./ApiHighlight";
 
 export default interface ApiInstitution {
   data: ApiInstitutionData;
   score: number;
-  highlight: ApiHighlight;
+  highlight: any;
 }
