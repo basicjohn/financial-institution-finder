8d28ff088179501f54cfba870724fbaea8809ebb
basicjohn <edmondsonj@gmail.com>
2022-01-10 21:55:21 -0800

    add updated App & Index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 // Dependencies
 import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
 
 // Styles
 import './App.css';
 
 // Components
+import InstitutionsList from './components/InstitutionsList';
+import InstitutionDetail from './components/InstitutionDetail';
 
 
-function App() {
+export const App:React.FC = () => {
+
   return (
     <div className="App">
-      <p>testing</p>
+      <Router>
+        <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<InstitutionsList />} />
+              <Route path="/institutions" element={<InstitutionsList />} />
+              <Route path="/institutions/:id"element={<InstitutionDetail />} />
+            </Routes>
+          </main>
+      </Router>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,8 @@ import App from './App';
 
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
